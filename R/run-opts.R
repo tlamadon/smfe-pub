da880ef39f863649859eb4d995ce24c78130903f
Bradley Setzler <bradley.setzler@gmail.com>
2022-02-17 10:53:33 -0500

    Remove deprecated options

--- R/run-opts.R
+++ R/run-opts.R
@@ -2,7 +2,7 @@
 
 # --- SETTING COUNTRY OPTIONS ----
 
-#' get valid suffix set for a given country
+#' get valid suffix set for a given country, perhaps with additional specifications
 #' @export
 Do.Country.Suffix <- function(country){
 
@@ -18,7 +18,7 @@ Do.Country.Suffix <- function(country){
   if(substr(country,1,3)=='US-'){
     if(country %in% c('US-early','US-late')){
       suffix.set$AKM <- c("_connected","_leaveout")
-      #suffix.set$CRE <- c("_connected_clusters10","_all_clusters10",paste0("_connected_clusters",2:5*10))
+      suffix.set$CRE <- c("_connected_clusters10","_all_clusters10",paste0("_connected_clusters",2:5*10))
       suffix.set$CRE <- c("_connected_clusters10","_leaveout_clusters10")
       suffix.set$TraceHO <- NA
       suffix.set$TraceHE <- NA
@@ -41,7 +41,6 @@ Do.Country.Suffix <- function(country){
     # for states, use defaults
   }
 
-
   # AT, IT, IT_Kline
   if((country=='AT') | (country=='IT') | (country=='IT_Kline')){
     # use defaults
@@ -57,16 +56,12 @@ Do.Country.Suffix <- function(country){
     # use defaults
   }
 
-  if(grepl('educgender',country)){
-    suffix.set$CRE <- c(suffix.set$CRE,"_connected_clusters30")
-  }
-
   return(suffix.set)
 
 }
 
 
-#' get valid directory paths for a given country
+#' get valid directory paths for a given country, perhaps with additional specifications
 #' @export
 Do.Country.Paths <- function(country){
 
@@ -76,17 +71,19 @@ Do.Country.Paths <- function(country){
   # US--------------------------------------------------
   if (substr(country,1,3)=='US-'){
     paths$raw <- "~/manageCDW/raw_data/"
-    if(country %in% c('US-early','US-late','US-early-noendpoints','US-late-noendpoints')){
-      library(akm)
-      library(paneltools)
-      library(parallel)
+    if(country %in% c('US-early','US-late','US-early-noendpoints','US-late-noendpoints')){ # these are the only available US national samples
+      # packages: due to the massive size of the US data, additional packages are required to handle it.
+      library(akm) # this package provides a memory-efficient implementation of the AKM twoway-FE estimator
+      library(paneltools) # this package includes a memory-efficient way to residualize the outcome variable on covariates
+      library(parallel) # this package allows us to run certain steps in parallel
       dir.create(file.path("~/bhlmms/national"), showWarnings = FALSE)
       paths$data <- sprintf("~/bhlmms/national/%s/jdata_sdata/",country)
       paths$res <- sprintf("~/bhlmms/national/%s/results/",country)
       paths$final <- "~/bhlmms/national/final-output/"
       paths$attrition <- sprintf("~/bhlmms/national/%s/attrition/",country)
       dir.create(file.path(sprintf("~/bhlmms/national/%s",country)), showWarnings = FALSE)
-    } else {
+    } else { # if not using a US national sample, it must be using a US state-specific subsample.
+      # note: no additional packages are needed since the state-specific samples are small.
       dir.create(file.path("~/bhlmms/states"), showWarnings = FALSE)
       paths$data <- sprintf("~/bhlmms/states/%s/jdata_sdata/",country)
       paths$res <- sprintf("~/bhlmms/states/%s/results/",country)
@@ -96,39 +93,16 @@ Do.Country.Paths <- function(country){
     }
   }
 
-  # AT, IT, ES-------------------------------------------------------
-  if ((country %in% c('ES')) | ((grepl('AT', country))) | ((grepl('IT', country)))){
-      if (!(country %in% c('AT', 'IT'))) {
+  # AT, IT-------------------------------------------------------
+  if  (grepl('AT', country) | grepl('IT', country)){
       setwd(path)
       proj_path=dirname(dirname(path))
-      paths$samples  = paste0(proj_path, "/Data_", substr(country,1,2), "/Samples/")
-      dir.create(paths$samples, showWarnings = FALSE)
-      paths$data <- paste0(paths$samples,'smfe-res-',country, '/')
-      paths$res  = paste0(proj_path, "/Results/", substr(country,1,2), "/")
-      paths$raw  = paste0(proj_path, "/Data_", substr(country,1,2), "/")
-      dir.create(paths$data, showWarnings = FALSE)
-    } else {
-      setwd(path)
-      proj_path=dirname(dirname(path))
-      paths$connectivity  = paste0(proj_path, "/Data_", substr(country,1,2), "/Yearly/")
-      paths$res  = paste0(proj_path, "/Results/", country, "/")
-      paths$data  = paste0(proj_path, "/Data_", country, "/")
-      paths$raw  = paste0(proj_path, "/Data_", country, "/")
-    }
-      if ((grepl('IT', country)) & (grepl('ITKline', country)==0) ){
-            paths$raw<-paste0(proj_path, "/Data_IT/1985_2001_veneto_only.dta")
-      } else if ((grepl('ITKline', country))) {
-           if (grepl('6', country)) {
-              paths$raw<-paste0(proj_path, "/Data_IT/All_Veneto_1996and2001_T_greater_2.dta") #
-           } else {
-            paths$raw<-paste0(proj_path, "/Data_IT/All_Veneto_1999and2001_T_equal_2.dta") #
-           }
+      if grepl('IT', country) {
+         paths$raw<-paste0(proj_path, "/Data_IT/py_final_1985_2001_veneto_only.dta")
       } else if ((grepl('AT', country))) {
-            paths$raw<-paste0(proj_path, "/Data_AT/Raw/anadata.dta")
-      } else if (country =="ES") {
-            paths$raw<-paste0(paths$data, "mcvl_cleaned.dta")
+         paths$raw<-paste0(proj_path, "/Data_AT/Raw/anadata.dta")
       }
-    paths$prog  = paste0(proj_path, "/smfe//smfe/R/") #where the programs are in
+    paths$prog  = paste0(proj_path, "/smfe/smfe/R/") #where the programs are in
     paths$call  = paste0(proj_path, "/smfe/smfe/inst/server-scripts/") # where the run script is in
     paths$pkg  = paste0(proj_path, "/Packages/") #where the packages are stored
     paths$files  = paste0(proj_path, "/Files/") #where the packages are being installed in
@@ -136,35 +110,15 @@ Do.Country.Paths <- function(country){
     paths$attrition=paths$res
   }
 
-  # IT, tibo working in a different folder --------------------
-  if (substr(country,1,2) == 'IY'){
-    proj_path = "~/git"
-    paths$samples  = paste0(proj_path, "/Data_", substr(country,1,2), "/Samples/")
-    paths$res   = paste0(proj_path, "/Results/", substr(country,1,2), "/")
-    paths$raw   = paste0("~/shared/smfe/data.dta")
-    paths$prog  = paste0(proj_path, "/smfe/smfe/R/") #where the programs are in
-    paths$call  = paste0(proj_path, "/smfe/smfe/inst/server-scripts/") # where the run script is in
-    paths$final = paste0(proj_path, "/smfe/smfe/inst/local-res/")
-
-    paths$data <- paste0(proj_path,'/smfe-res-',country, '/')
-    paths$attrition=paths$res
-
-    dir.create(paths$data, showWarnings = FALSE)
-    dir.create(paths$samples, showWarnings = FALSE)
-  }
-
   # SW------------------------------------------------------------------------
   if (substr(country,1,2) == "SW") {
-#     library(akm)
     paths$data  <- sprintf("../smfe-res-%s/",country)
     paths$res   <- sprintf("../smfe-res-%s/",country)
     paths$final <- sprintf("../smfe-res-%s/",country)
     paths$attrition   <- "../tmp/"
   }
-
   # Sweden for attrition
   if (country=='SW-attrition'){
-#     library(akm)
     paths$data  <- "../tmp2/data/"
     paths$res   <- "../tmp2/tmp/"
     paths$final <- "../tmp2/final/"
@@ -211,8 +165,6 @@ Do.Country.Paths <- function(country){
     paths$final <- "/ssb/stamme01/swp/lae/wk48/labor/NO3thresh_annualearnings/results/"
     paths$attrition <- "/ssb/stamme01/swp/lae/wk48/labor/NO3thresh_annualearnings/attrition/"
   }
-
-
   if (country=='NO6brad-annualearnings'){
     paths$data  <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings/jdata_sdata/"
     paths$res   <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings/intermediate_output/"
@@ -220,71 +172,15 @@ Do.Country.Paths <- function(country){
     paths$attrition <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings/attrition/"
   }
 
-  if (country=='NO6brad-annualearnings-educgender'){
-    paths$data  <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educgender/jdata_sdata/"
-    paths$res   <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educgender/intermediate_output/"
-    paths$final <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educgender/final_output/"
-    paths$attrition <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educgender/attrition/"
-  }
-  if (country=='NO6brad-annualearnings-educindustry'){
-    paths$data  <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educindustry/jdata_sdata/"
-    paths$res   <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educindustry/intermediate_output/"
-    paths$final <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educindustry/final_output/"
-    paths$attrition <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educindustry/attrition/"
-  }
 
-  if (country=='NO6brad-annualearnings-industry'){
-    paths$data  <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_industry/jdata_sdata/"
-    paths$res   <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_industry/intermediate_output/"
-    paths$final <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_industry/final_output/"
-    paths$attrition <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_industry/attrition/"
-  }
-  if (country=='NO6brad-annualearnings-educ'){
-    paths$data  <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educ/jdata_sdata/"
-    paths$res   <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educ/intermediate_output/"
-    paths$final <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educ/final_output/"
-    paths$attrition <- "/ssb/stamme01/swp/bjs/wk24/private/BHLMMS/NO6brad_annualearnings_educ/attrition/"
-  }
-
-
-
-
-
-  #DE----------------------------------------------------------------
-    if ((country=='DE') | (country=='DE-test')) {
-    if (country=='DE'){
-    paths$res  = "H:/log/"
-    paths$pkg  = "//N0016017/usr/public/R-Packages/3.4/" #local pkg depository
-    paths$files  = "H:/prog/"      #where to install the pkgs
-    paths$call  = opts$paths$files                     #where the scripts (03_Main.R) are
-    paths$prog  = opts$paths$files                     #where the function (other_functions.R) files are
-    paths$data  = "H:/data/"
-    paths$final=paths$res
-    paths$attrition=paths$res
-    }
-   if (country=='DE-test'){
-    setwd(path)
-    proj_path=dirname(path)
-    paths$res  = paste0(dirname(proj_path), "/Results/")
-    paths$pkg  = paste0(dirname(proj_path), "/Packages/")
-    paths$files  = paste0(dirname(proj_path), "/Files/") #where the packages are being installed in
-    paths$prog  = paste0(proj_path, "/R/")
-    paths$data  = paste0(dirname(proj_path), "/Data_DE/")
-    paths$final=paths$res
-    paths$attrition=paths$res
-
-    }
-   }
-
-    # Testing Brad--------------------------------------------
-  if (country=='Testing-Brad'|country=='Testing-Claire'){
+  # Testing on Brad's local machine--------------------------------------------
+  if (country=='Testing-Brad'){
     paths$data <- "~/Downloads/smfe-temp/jdata_sdata/"
     paths$res <- "~/Downloads/smfe-temp/results/"
     paths$final <- "~/Downloads/smfe-temp/final-output/"
     paths$attrition <- "~/Downloads/smfe-temp/attrition/"
     dir.create(file.path("~/Downloads/smfe-temp"), showWarnings = FALSE)
   }
-
 
   # make sure directories exist
   dir.create(file.path(paths$data), showWarnings = FALSE)
@@ -326,12 +222,6 @@ Do.Country.Attrition <- function(country){
     #default
   }
 
-
-  # DE
-  if(country=='DE'|country=='DE-test'){
-    #default
-  }
-
   # AT, IT
   if (country=='AT'| country=='IT' ){
     #default
@@ -358,18 +248,18 @@ Do.Country.Attrition <- function(country){
 Do.Country.Misc <- function(country){
 
   # defaults
-  misc <- list(ncores=1,
-               nstart=100,
-               preserve.estimates=TRUE,
-               preserve.initdata=TRUE,
-               preserve.rawdata=TRUE,
-               cre.sub=FALSE,
-               posterior_var=TRUE,
+  misc <- list(ncores=1, # number of cores to use in parallelization
+               nstart=100, # number of random starts when finding k-means clusters
+               preserve.estimates=TRUE, # do not overwrite existing estimates if TRUE
+               preserve.initdata=TRUE, # do not overwrite existing estimates if TRUE
+               preserve.rawdata=TRUE, # do not overwrite existing estimates if TRUE
+               cre.sub=FALSE, # use a random subsampling approach in CRE estimation if TRUE
+               posterior_var=TRUE, # estimate the posterior variance in CRE estimation if TRUE
                mover_cluster_max_share=0.1,
-               data_only=FALSE,
+               data_only=FALSE, # do not perform any FE estimation if TRUE, simply save the intermediate data sets for later analyses
                connectivity=FALSE,
-               cluster_bothyears=FALSE,
-               grouping_var='wages'
+               cluster_bothyears=FALSE, # use both pre-move and post-move data when estimating clusters if TRUE
+               grouping_var='wages' # estimate the clusters based on this variable
                )
 
   # US
@@ -390,26 +280,12 @@ Do.Country.Misc <- function(country){
     misc$posterior_var <- TRUE
   }
 
+  # If manually specifying the begin_year and end_year, that can be set here
   if(grepl('select',country)){
      year_set = sub(".*select_", "", country)
      misc$begin_year = as.integer(substr(year_set,1,4))
      misc$end_year = as.integer(substr(year_set,6,9))
   }
-
-
-  if(grepl('educ',country)){
-    misc$grouping_var='educ'
-  }
-  if(grepl('industry',country)){
-    misc$grouping_var='industry'
-  }
-  if(grepl('educindustry',country)){
-    misc$grouping_var='educindustry'
-  }
-  if(grepl('educgender',country)){
-    misc$grouping_var='educgender'
-  }
-
 
   return(misc)
 
@@ -431,6 +307,5 @@ Do.Init.Opts <- function(country){
 
   return(opts)
 }
-
 
 
